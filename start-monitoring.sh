6f0a8481773fba98cd7ef52156abd0823f9515d5
hejnaluk <kejhy93@gmail.com>
2025-08-19 19:32:33 +0200

    Detect OS and add-host parameter if necessary on Linux

--- start-monitoring.sh
+++ start-monitoring.sh
@@ -18,6 +18,27 @@ else
   fi
 fi
 
+# Detect OS type
+OS="$(uname -s)"
+ADD_HOST_FLAG=""
+
+if [ "$OS" = "Linux" ]; then
+  echo "Running on Linux → resolving Docker host gateway IP"
+  # Try to detect the default gateway IP used by docker0
+  if [[ "$OS" == "Linux" ]]; then
+    # Try Docker bridge gateway
+    HOST_IP=$(ip route | grep docker0 | awk '{print $9}' || true)
+
+    # Fallback to default gateway
+    if [[ -z "$HOST_IP" ]]; then
+        HOST_IP=$(ip route | grep '^default' | awk '{print $3}')
+    fi
+  ADD_HOST_FLAG="--add-host=host.docker.internal:$HOST_IP"
+  echo "Adding host mapping: host.docker.internal -> $HOST_IP"
+  fi
+else
+  echo "Running on $OS → no extra host mapping needed"
+fi
 
 PROMETHEUS_CONTAINER_NAME="prometheus"
 # Check if container is already running
@@ -30,12 +51,13 @@ else
     docker start "$PROMETHEUS_CONTAINER_NAME"
   else
     echo "Running new container '$PROMETHEUS_CONTAINER_NAME'..."
-    docker run -d -p 9090:9090 --name=prometheus -v ~/prometheus/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus
+    # docker run -d -p 9090:9090 --name=prometheus -v ~/monitoring/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus
     docker run -d \
 	    -p 9090:9090 \
-	    -v ~/prometheus/prometheus.yml:/etc/prometheus/prometheus.yml \
+	    -v ~/monitoring/prometheus.yml:/etc/prometheus/prometheus.yml \
 	    --name "$PROMETHEUS_CONTAINER_NAME" \
 	    --network "$NETWORK_NAME" \
+      $ADD_HOST_FLAG \
 	    prom/prometheus
   fi
 fi
